50415b7d676ff7e1f5f1ed5a8ab951f2785bc8fa
Gabriel Vidal <gabvidtev@alum.us.es>
2023-03-17 11:33:26 +0100

    Individual task student #3 fixed

--- src/main/java/acme/datatypes/sessions/Tutorial.java
+++ src/main/java/acme/datatypes/sessions/Tutorial.java
@@ -3,12 +3,16 @@ package acme.datatypes.sessions;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Assistant;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -39,5 +43,19 @@ public class Tutorial extends AbstractEntity {
 	@NotBlank
 	@Length(min = 0, max = 100)
 	protected String			goals;
+
+	protected Double			estimatedTotalTime;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Assistant			assistant;
+
+	//@NotNull
+	//@Valid
+	//@ManyToOne(optional = false)
+	//protected course			course;
 
 }
