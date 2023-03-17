07845a688c22cd59f28a8c8368928ea558a27d9d
javulegar <javiulecia@gmail.com>
2023-03-17 20:03:26 +0100

    individual tasks

---
+++ src/main/java/acme/entities/notes/notes.java
@@ -0,0 +1,46 @@
+
+package acme.entities.notes;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class notes extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			author;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			message;
+	@Email
+	protected String			email;
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
